a7d1ae7c08f8afc61b56f030469894ae7414bb3c
Jkhan3232 <jeeshankhan3232@gmail.com>
2026-04-16 10:03:18 +0530

    git push

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://naomedical-ai-native-full-stack-dev-ten.vercel.app/";
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://naomedical-ai-native-full-stack-dev-ten.vercel.app";
 
 const apiClient = axios.create({
   baseURL: `${API_BASE_URL}/api`,
